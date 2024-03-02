5c7abe036c16eafcfd2ba437c45f8bd0cb0d2b9c
Asim Rasheed <asimrasheed442@gmail.com>
2024-03-02 10:51:34 +0500

    update

--- App.js
+++ App.js
@@ -1,24 +1,4 @@
 import AppNavigation from "./src/navigation";
-import {
-  View,
-  Text,
-  Image,
-  TouchableOpacity,
-  SafeAreaView,
-} from "react-native";
 export default function App() {
   return <AppNavigation />;
-  // return (
-  //   <>
-  //     <View
-  //       className="
-  //       flex-1
-  //       items-center
-  //       justify-center
-  //     "
-  //     >
-  //       <Text>Test</Text>
-  //     </View>
-  //   </>
-  // );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
-// tailwind.config.js
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
+
   theme: {
     extend: {},
   },
